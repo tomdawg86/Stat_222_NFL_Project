b7e8f7492730e4581d7be0395ad38c2f1faf44c7
Thomas McCann <tomdawg86@berkeley.edu>
2014-04-12 13:16:29 -0700

    nothing major

--- ipythonblocks-1.6.0/ipythonblocks/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-ipythonblocks provides a BlockGrid class that displays a colored grid in the
-IPython Notebook. The colors can be manipulated, making it useful for
-practicing control flow stuctures and quickly seeing the results.
-
-"""
-
-from .ipythonblocks import *
